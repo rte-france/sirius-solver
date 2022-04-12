9599378f904328ae6d7553b7935d62b34ab3e9cb
Florian OMNES <florian.omnes@rte-france.com>
2022-04-12 18:29:13 +0200

    Update 2007-2018 -> 2007-2022

--- src/PNE/pne_heuristique_resolution_branch_and_bound_reduit.c
+++ src/PNE/pne_heuristique_resolution_branch_and_bound_reduit.c
@@ -1,4 +1,4 @@
-// Copyright (C) 2007-2018, RTE (https://www.rte-france.com)
+// Copyright (C) 2007-2022, RTE (https://www.rte-france.com)
 // See AUTHORS.txt
 // SPDX-License-Identifier: Apache-2.0
 
